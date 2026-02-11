e1f37110434685e5886934263ae68195d64d4ae5
Dipanshu Sharma <sharmadipanshu1904@gmail.com>
2026-02-11 11:24:36 +0530

    [Dipanshu Sharma] Add. add the solution of banking testing problem

---
+++ collections-practice/gcr-codebase/junit-practice/src/test/java/UnitTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class UnitTest {
+    @Test
+    public void Test_Deposit_ValidAmount() {
+        Program acc=new Program(100);
+        acc.deposit(50);
+        assertEquals(150,acc.getBalance());
+    }
+
+    @Test
+    public void Test_Deposit_NegativeAmount() {
+        Program acc=new Program(100);
+        Exception ex=assertThrows(RuntimeException.class,()->acc.deposit(-20));
+        assertEquals("Deposit amount cannot be negative",ex.getMessage());
+    }
+
+    @Test
+    public void Test_Withdraw_ValidAmount() {
+        Program acc=new Program(200);
+        acc.withdraw(80);
+        assertEquals(120,acc.getBalance());
+    }
+
+    @Test
+    public void Test_Withdraw_InsufficientFunds() {
+        Program acc=new Program(100);
+        Exception ex=assertThrows(RuntimeException.class,()->acc.withdraw(150));
+        assertEquals("Insufficient funds.",ex.getMessage());
+    }
+}
